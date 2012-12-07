d36ce1cc41c810ad41259f5da1cf87266398816c
Drew Butler <abutler3@gmail.com>
2012-12-07 08:50:45 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Starlinks::Application.config.secret_token = '26a2c914edf07f80885534c5c5f56b2d6c973fe7891dd11c9ed986ba41c9e516c27954a84f3d63ab53cc3f5d9ccbfba8299aa74f440d9005e58db3a078da5ae3'
