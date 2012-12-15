6f25f75efe276d28f3335655fccf58dec2941967
Drew Butler <abutler3@gmail.com>
2012-12-15 08:04:58 -0600

    Add comments to view for specific links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,6 +3,7 @@ class LinksController < ApplicationController
   def show
 
     @link = Link.find(params[:id])
+    @comment = Comment.new
 
     respond_to do |format|
       format.html # show.html.erb
