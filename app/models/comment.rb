11faa402148e2fc6a60e1620f72424b5b03eac41
Drew Butler <abutler3@gmail.com>
2012-12-15 18:12:10 -0600

    Create vote model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :message, :user_id, :link_id
+  attr_accessible :up, :user_id, :link_id
 
   belongs_to :link
   belongs_to :user
